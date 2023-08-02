cd55cff94eb2c75a8adf3d8c5f18775fa3052e6f
tanish <tanish.afre@gmail.com>
2023-08-02 11:42:12 +0100

    finxing errors

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    fitstname:{
+    firstname:{
         type:String,
         required:true,
     },
