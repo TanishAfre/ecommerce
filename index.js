a4d160e9e338152600096c7ea78ed0526ee8e957
tanish <tanish.afre@gmail.com>
2023-08-02 11:28:42 +0100

    setting route

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const dbConnect = require('./config/dbConnect');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
-
+const authRouter = require('./routes/authRoutes');
 dbConnect();
 
 app.use("/", (req,res) => {
     res.send("Hello from server side");
 });
+
+app.use('/api/user', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server us running at port ${PORT}`);
