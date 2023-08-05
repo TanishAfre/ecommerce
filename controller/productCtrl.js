ae45e821ab850d6366345fe39db634b539edab15
tanish <tanish.afre@gmail.com>
2023-08-05 15:49:49 +0100

    get product working

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -12,5 +12,14 @@ const createProduct = asyncHandler(async (req, res) => {
     
 });
 
+const getaProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const findProduct = await Product.findById(id);
+        res.json(findProduct);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
 
-module.exports = { createProduct };
+module.exports = { createProduct, getaProduct };
