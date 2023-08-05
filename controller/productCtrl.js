b4dff48fa335fe6e32ae976811a508b75c6ed7ca
tanish <tanish.afre@gmail.com>
2023-08-05 22:15:04 +0100

    Limiting fields

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -37,6 +37,15 @@ const getAllProduct = asyncHandler(async (req, res) => {
             query = query.sort("-createdAt");
         }
 
+
+        // Limiting the fields
+        if (req.query.fields){
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields);
+        } else {
+            query = query.sort("-__v");
+        }
+
         const product = await query;
         res.json(product);
     } catch (error) {
