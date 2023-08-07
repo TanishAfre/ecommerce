83eac5fc3d7aa5a377b11914939297babf572022
tanish <tanish.afre@gmail.com>
2023-08-07 19:34:09 +0100

    setting up mail

---
+++ controller/emailCtrl.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+const asyncHandler = require("express-async-handler");
+
+
+const sendEmail = asyncHandler(async( data, req, res ) => {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+          user: process.env.MAIL_ID,
+          pass: process.env.MP,
+        }
+      });
+
+
+      // Send Mail with defined transport object
+      const info = await transporter.sendMail({
+        from: '"Hey 👻" <abc@gmail.com>', // sender address
+        to: data.to, // list of receivers
+        subject: data.subject, // Subject line
+        text: data.text, // plain text body
+        html: data.html, // html body
+      });
+
+      console.log("Message sent: %s", info.messageId);
+
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+});
+
+module.exports = { sendEmail };
