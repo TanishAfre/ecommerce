aa85c25c54f97084c0945f3630a41baf9c9539a0
tanish <tanish.afre@gmail.com>
2023-08-09 13:02:59 +0100

    update and create blog working

---
+++ controller/blogCtrl.js
@@ -0,0 +1,42 @@
+const Blog = require("../models/blogModel");
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utiles/validateMongodbid");
+
+
+
+const createBlog = asyncHandler(async (req, res) => {
+    try {
+        const newBlog = await Blog.create(req.body);
+        res.json(newBlog);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const updateBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+            new : true,
+        });
+        res.json(updateBlog);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const updateBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+            new : true,
+        });
+        res.json(updateBlog);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+
+module.exports = { createBlog, updateBlog };
