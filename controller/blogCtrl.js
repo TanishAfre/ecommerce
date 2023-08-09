ff25fe1e11d82c65726ec8bfc5dad4724d2c984f
tanish <tanish.afre@gmail.com>
2023-08-09 14:15:45 +0100

    get blog working with increase number of views

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -29,10 +29,13 @@ const updateBlog = asyncHandler(async (req, res) => {
 const getBlog = asyncHandler(async (req, res) => {
     const { id } = req.params;
     try {
-        const getBlog = await Blog.findById(id, req.body, {
-            new : true,
-        });
-        res.json(getBlog);
+        const getBlog = await Blog.findById(id);
+        const updateViews = await Blog.findByIdAndUpdate(id, {
+            $inc: { numViews: 1 },
+        },
+            { new: true }
+        );
+        res.json(updateViews);
     } catch (error) {
         throw new Error(error);
     }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createBlog, updateBlog } = require("../controller/blogCtrl");
+const { createBlog, updateBlog, getBlog } = require("../controller/blogCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
 router.post("/", authMiddleware, isAdmin, createBlog);
 router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.get("/:id", getBlog);
 module.exports = router;
