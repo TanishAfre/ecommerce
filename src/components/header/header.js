d1ef8578421875b2dc3fa6a941eac52b76c8835b
tanish <tanish.afre@gmail.com>
2024-06-04 10:13:32 +0100

    :hammer: adding search bar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "./header.css";
+import Logo from "../../assets/images/ecommerceBrandLogo.jpg";
+import SearchIcon from "@mui/icons-material/Search";
+import Select from "../selectDrop/select"
 
 const Header = () => {
   return (
-      <>
-      
-      </>
+      <header>
+        <div className="container-fluid">
+            <div className="row">
+                {/* Logo */}
+                <div className="col-sm-2">
+                    <img src={Logo} alt="Logo" className="logo" />
+                </div>
+
+                {/* header search starts here */}
+                <div className="col-sm-6">
+                    <div className="headerSearch d-flex align-item-center">
+                        <div className="selectDrop cursor">
+                            All Catagories
+                            <Select />
+                        </div>
+
+                        <div className="searchBox">
+                            <input type="text" placeholder="Search for fresh fruits or vegetables" />
+                            <SearchIcon className="searchIcon cursor" />
+                        </div>
+                    </div>
+                </div>
+                {/* header search ends here */}
+            </div>
+        </div>
+      </header>
   );
 }
 
---
+++ src/components/selectDrop/select.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../selectDrop/select.css"
+
+const Select = () => {
+    return (
+        <div className="selectDrop cursor">
+            <div className="searchField">
+                <input type="text" />
+            </div>
+        </div>
+    );
+}
+
+export default Select;
