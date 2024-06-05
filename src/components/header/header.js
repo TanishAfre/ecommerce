0e7225901d5f4cb1bd8d6a183fe09c45dd7a73a0
tanish <tanish.afre@gmail.com>
2024-06-05 14:08:31 +0100

    :white_check_mark: Header Ready

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import Logo from "../../assets/images/ecommerceBrandLogo.jpg";
 import SearchIcon from "@mui/icons-material/Search";
 import Select from "../selectDrop/select"
+import axios from "axios";
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import CompareIcon from '@mui/icons-material/Compare';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+
 
 const Header = () => {
 
-    const [catagories, setCatagories] = useState([
-        'All Catagory', 
+    const [catagories, setCatagories] = useState([ 
         'Milk And Dairy',
         'Wine And Drinks',
         'Cloths And Beaity',
@@ -22,19 +28,43 @@ const Header = () => {
         'Iten 1'
     ]);
 
+    const countryList=[];
+
+    useEffect(() => {
+        getCountry('https://countriesnow.space/api/v0.1/countries')
+    }, []);
+
+    const getCountry = async (url) => {
+        try {
+            await axios.get(url).then((res) => {
+                if(res!==null){
+                    //console.log(res.data.data);
+                    res.data.data.map((item) => {
+                        countryList.push(item.country);
+                    });
+                }
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    
+
   return (
       <header>
         <div className="container-fluid">
             <div className="row">
                 {/* Logo */}
-                <div className="col-sm-2">
+                <div className="col-sm-1">
                     <img src={Logo} alt="Logo" className="logo" />
                 </div>
 
                 {/* header search starts here */}
-                <div className="col-sm-6">
+                <div className="col-sm-5 d-flex justify-content-center">
                     <div className="headerSearch d-flex align-item-center">
-                        <Select data={catagories}/>
+                        <Select data={catagories} placeholder={'All Catagory'} icon={false} />
+
                         <div className="searchBox">
                             <input type="text" placeholder="Search for fresh fruits or vegetables" />
                             <SearchIcon className="searchIcon cursor" />
@@ -43,9 +73,51 @@ const Header = () => {
                 </div>
                 {/* header search ends here */}
 
-                <div className="col-sm-5">
-                    <Select />
+                <div className="col-sm-4 d-flex aligh-items-center">
+                    <div className="ml-auto d-flex align-items-center rMost">
+                        <div className="countryWrapper cursor position-relative">
+                            <Select data={countryList} placeholder={'Your Location'} icon={<LocationOnOutlinedIcon style={{ opacity:'0.5' }}/>} />
+                        </div>
+
+                        <div className="list list-inline mb-0 headerTabs">
+                            <div className="list-inlist item">
+                                <span>
+                                    <CompareIcon />
+                                    <span className="badge bg-success rounded-circle">4</span>
+                                    Compare
+                                </span>
+                            </div>
+                        </div>
+                        <div className="list list-inline mb-0 headerTabs">
+                            <div className="list-inlist item">
+                                <span>
+                                    <FavoriteBorderIcon />
+                                    <span className="badge bg-success rounded-circle">4</span>
+                                    Wishlist
+                                </span>
+                            </div>
+                        </div>
+                        <div className="list list-inline mb-0 headerTabs">
+                            <div className="list-inlist item">
+                                <span>
+                                    <ShoppingCartOutlinedIcon />
+                                    <span className="badge bg-success rounded-circle">4</span>
+                                    Cart
+                                </span>
+                            </div>
+                        </div>
+                        <div className="list list-inline mb-0 headerTabs">
+                            <div className="list-inlist item">
+                                <span>
+                                    <PermIdentityOutlinedIcon />
+                                    <span className="badge bg-success rounded-circle">4</span>
+                                    Arrount
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
       </header>
--- src/components/selectDrop/select.js
+++ src/components/selectDrop/select.js
@@ -3,11 +3,12 @@ import "../selectDrop/select.css"
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { ClickAwayListener } from '@mui/base/ClickAwayListener';
 
-const Select = () => {
+
+const Select = ({data, placeholder, icon}) => {
 
     const [isOpenSelect, setIsOpenSelect] = useState(false);
     const [selectedIndex, setSelectedIndex] = useState(0);
-    const [selectedIten, setSelectedItem] = useState('All Catagories');
+    const [selectedIten, setSelectedItem] = useState(placeholder);
 
 
 
@@ -24,6 +25,7 @@ const Select = () => {
     return (
         <ClickAwayListener onClickAway={() => setIsOpenSelect(false)}>
             <div className="selectDropWrapper cursor position-relative">
+                {icon}
                 <span className="openSelect" onClick={openSelect} >{selectedIten}<ArrowDropDownIcon className="arrow" /></span>   
                 {
                     isOpenSelect===true && 
@@ -32,19 +34,15 @@ const Select = () => {
                             <input type="text" placeholder="Search here..."/>
                         </div>
                         <ul className="searchResults">
-                            <li onClick={()=>closeSelect(0, 'All Catagory')} className={`${selectedIndex===0 ? 'active':''}`}>All Catagory</li>
-                            <li onClick={()=>closeSelect(1, 'Milk And Dairy')} className={`${selectedIndex===1 ? 'active':''}`}>Milk And Dairy</li>
-                            <li onClick={()=>closeSelect(2, 'Wine And Drinks')} className={`${selectedIndex===2 ? 'active':''}`}>Wine And Drinks</li>
-                            <li onClick={()=>closeSelect(3, 'Cloths And Beaity')} className={`${selectedIndex===3 ? 'active':''}`}>Cloths And Beaity</li>
-                            <li onClick={()=>closeSelect(4, 'Fresh Sea Food')} className={`${selectedIndex===4 ? 'active':''}`}>Fresh Sea Food</li>
-                            <li onClick={()=>closeSelect(5, 'Vegetables')} className={`${selectedIndex===5 ? 'active':''}`}>Vegetables</li>
-                            <li onClick={()=>closeSelect(6, 'Fruits')} className={`${selectedIndex===6 ? 'active':''}`}>Fruits</li>
-                            <li onClick={()=>closeSelect(7, 'Iten 1')} className={`${selectedIndex===7 ? 'active':''}`}>Item 1</li>
-                            <li onClick={()=>closeSelect(8, 'Iten 1')} className={`${selectedIndex===8 ? 'active':''}`}>Item 1</li>
-                            <li onClick={()=>closeSelect(9, 'Iten 1')} className={`${selectedIndex===9 ? 'active':''}`}>Item 1</li>
-                            <li onClick={()=>closeSelect(10, 'Iten 1')} className={`${selectedIndex===10 ? 'active':''}`}>Item 1</li>
-                            <li onClick={()=>closeSelect(11, 'Iten 1')} className={`${selectedIndex===11 ? 'active':''}`}>Item 1</li>
-                            <li onClick={()=>closeSelect(12, 'Iten 1')} className={`${selectedIndex===12 ? 'active':''}`}>Item 1</li>
+                        <li key={0} onClick={()=>closeSelect(0, placeholder)} className={`${selectedIndex===0 ? 'active':''}`}>{placeholder}</li>
+                        {
+                            data.map((item, index) => {
+                                return (
+                                    <li key={index+1} onClick={()=>closeSelect(index+1, item)} className={`${selectedIndex===index+1 ? 'active':''}`}>{item}</li>
+                                )
+                            })
+                        }
+
                         </ul>
                     </div>
                 }
